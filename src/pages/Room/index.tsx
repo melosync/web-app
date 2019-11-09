58b82fa570554a353514d468a538cea117c2e514
Guillaume Benard <guillaume@benard.io>
2019-11-09 12:24:24 +0100

    Add dummy socket connection attempt

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,10 +1,26 @@
-import React from "react";
-import { Container } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import React, { useEffect } from "react";
+import SocketIo from "socket.io-client";
 
 import Styles from "./Room.module.scss";
 import VideoContainer from "./VideoContainer";
 
 const Room: React.FC = () => {
+  // TODO: Move in a dedicated file and connect real (token + room)
+  useEffect(() => {
+    const socket = SocketIo(process.env.REACT_APP_API_ENDPOINT!);
+
+    socket.on("connect", () => {
+      console.log("Connected");
+      socket.emit("authenticate", { token: "token", roomUuid: "room" });
+    });
+
+    socket.on("exception", (errorMessage?: string) => {
+      const message = errorMessage ? errorMessage : "An error occurred";
+      console.error(`Socket: ${message}`);
+    });
+  });
+
   return (
     <Container className={Styles.RoomContainer}>
       <VideoContainer />
